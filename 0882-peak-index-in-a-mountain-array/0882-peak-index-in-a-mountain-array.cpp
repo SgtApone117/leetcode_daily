b72267cff6cab78da4b330072d4fdbf8d6b68861
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2023-10-11 18:18:21 -0700

    Time: 92 ms (23%), Space: 60.1 MB (11.38%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l = 0;
+        int r = arr.size() - 1;
+        while(l < r)
+        {
+            int mid = l + (r-l)/2;
+            if(arr[mid] < arr[mid+1])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid;
+            }
+        }
+        return l;
+    }
+};
