fe5522bcc3018a2ef7fea2cf5dc785dfa4e81e6c
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2023-12-24 13:51:57 -0800

    Time: 13 ms (31.02%) | Memory: 10.9 MB (81.80%) - LeetSync

---
+++ 1547-destination-city/destination-city.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_set<string> st;
+
+        for(auto& path : paths)
+        {
+            string source = path[0];
+            st.insert(source);
+        }
+
+        for(auto& path : paths)
+        {
+            string dest = path[1];
+
+            if(st.find(dest) == st.end())
+            {
+                return dest;
+            }
+        }
+        return "";
+    }
+};
