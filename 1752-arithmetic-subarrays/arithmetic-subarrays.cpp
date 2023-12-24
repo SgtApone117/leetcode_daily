70cd799c679c55836a2c8c805694bc9dfbebed15
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2023-12-24 13:43:09 -0800

    Time: 40 ms (76.53%) | Memory: 26.2 MB (74.62%) - LeetSync

---
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool check(vector<int> & arr)
+    {
+        sort(begin(arr), end(arr));
+
+        int n = arr.size();
+        int d = arr[1] - arr[0];
+
+        for(int i = 2; i <n; i++)
+        {
+            if(arr[i] - arr[i-1] != d)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) 
+    {
+        int n = l.size();
+
+        vector<bool> result;
+        for(int i = 0; i < n; i++)
+        {
+            int start = l[i];
+            int end = r[i];
+
+            vector<int> arr(begin(nums) + start, begin(nums) + end + 1);
+
+            bool isAp = check(arr);
+
+            result.push_back(isAp);
+        }
+        return result;
+    }
+};
