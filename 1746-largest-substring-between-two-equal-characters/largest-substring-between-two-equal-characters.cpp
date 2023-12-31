0f1c53065b0b2baeca033d1e4c220cc67bb696cb
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2023-12-30 22:41:32 -0800

    Time: 6 ms (5.74%) | Memory: 6.7 MB (41.15%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -5,7 +5,7 @@ public:
 
         int result = -1;
 
-        for(int i = 0; i <= n-1; i++)
+        for(int i = 0; i <= n-2; i++)
         {
             for(int j = i+1; j < n; j++)
             {
