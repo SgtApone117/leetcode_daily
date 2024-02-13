de85bc8fb82a77692c940bfd92a87ab3258b97c8
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-02-13 10:12:59 -0800

    Time: 4 ms (93.29%) | Memory: 18.2 MB (39.42%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,37 +2,51 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) 
     {
-        int count1 = 0, count2 = 0, el1, el2, n = (int)nums.size();
-        for(int i = 0; i < n; i++)
+        vector<int> result;
+        int candidate1 = INT_MIN, candidate2 = INT_MIN, vote1 = 0, vote2 = 0;
+        for(int &num : nums)
         {
-            if(count1 == 0 && nums[i] != el2)
+            if(0 == vote1 && num != candidate2)
             {
-                el1 = nums[i];
-                count1 = 1;
+                vote1++;
+                candidate1 = num;
             }
-            else if(count2 == 0 && nums[i] != el1)
+            else if(0 == vote2 && num != candidate1)
             {
-                el2 = nums[i];
-                count2 = 1;
+                vote2++;
+                candidate2 = num;
             }
-            else if(el1 == nums[i]) count1++;
-            else if(el2 == nums[i]) count2++;
+            else if(num == candidate1)
+            {
+                vote1++;
+            }
+            else if(num == candidate2)
+            {
+                vote2++;
+            }
             else
             {
-                count1--;
-                count2--;
+                vote1--;
+                vote2--;
             }
         }
-        int cnt1 = 0, cnt2 = 0, maj_condition = (int)(n / 3) + 1;
-        vector<int> vec;
-        for(int i = 0; i < n; i++)
+        int mini = int(nums.size() / 3) + 1;
+        vote1 = 0, vote2 = 0;
+        for(int &x : nums)
         {
-            if(el1 == nums[i]) cnt1++;
-            if(el2 == nums[i]) cnt2++;
-        }   
-        if(cnt1 >= maj_condition) vec.emplace_back(el1);
-        if(cnt2 >= maj_condition) vec.emplace_back(el2);
-        sort(vec.begin(),vec.end());
-        return vec;
+            if(x == candidate1)
+                vote1++;
+            else if(x == candidate2)
+                vote2++;
+        }
+        if(vote1 >= mini)
+        {
+            result.push_back(candidate1);
+        }
+        if(vote2 >= mini)
+        {
+            result.push_back(candidate2);
+        }
+        return result;
     }
 };
