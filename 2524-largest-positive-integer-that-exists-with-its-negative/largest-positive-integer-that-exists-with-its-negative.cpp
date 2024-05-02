ba4a172d8fa579781754b415d7a6d74807fb32f5
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-05-02 16:42:52 -0700

    Time: 11 ms (94.06%) | Memory: 22.8 MB (79.58%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-        // Sort the input array in ascending order
         sort(begin(nums), end(nums));
-
-        int lo = 0;
-        int hi = size(nums) - 1;
-        
-        while (lo < hi) {
-
-            // If the negation of the element at lo is equal to the element at hi
-            if (-nums[lo] == nums[hi]) {
-                return nums[hi];
-            } 
-            
-            // If the negation of element at lo is greater than element at hi
-            else if (-nums[lo] > nums[hi]) { 
-                ++lo;
+        int high = nums.size() - 1;
+        int low = 0;
+        while(high > low)
+        {
+            if(nums[high] == -nums[low])
+                return nums[high];
+            else if(-nums[low] > nums[high])
+            {
+                ++low;
             }
-            
-            // If the negation of element at lo is smaller than element at hi
-            else { 
-                --hi;
+            else
+            {
+                --high;
             }
         }
         return -1;
