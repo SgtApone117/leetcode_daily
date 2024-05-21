3c62834f148101d380a07fa0d6b093cf1ea9feac
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-05-21 07:22:30 +0530

    Time: 4 ms (55.12%) | Memory: 8.2 MB (88.34%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, int i, vector<int>& currSubset, vector<vector<int>>& subSets)
+    int solve(vector<int>& nums, int i, int Xor)
     {
         if(i == nums.size())
         {
-            subSets.push_back(currSubset);
-            return;
+            return Xor;
         }
+        int include = solve(nums, i+1, nums[i]^Xor);
+        int exclude = solve(nums, i+1, Xor);
 
-        currSubset.push_back(nums[i]);
-        solve(nums, i+1, currSubset, subSets);
-        currSubset.pop_back();
-        solve(nums, i+1, currSubset, subSets);
+        return include+exclude;
     }
-    int subsetXORSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> subSets;
-        vector<int> currSubset;
-
-        solve(nums, 0, currSubset, subSets);
-
-        int result = 0;
-        for(vector<int> &subSet : subSets)
-        {
-            int Xor = 0;
-            for(int &num : subSet)
-            {
-                Xor ^= num;
-            }
-            result += Xor;
-        }
-        return result;
+    int subsetXORSum(vector<int>& nums) 
+    {
+        return solve(nums, 0, 0);
     }
 };
