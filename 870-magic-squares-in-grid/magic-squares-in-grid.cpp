120a0db1e8ab5384a464807f2e9f9ee47efd9590
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-08-10 05:14:02 +0530

    Time: 5 ms (32.93%) | Memory: 12.2 MB (19.51%) - LeetSync

---
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool isMagicGrid(vector<vector<int>>& grid, int r, int c)
+    {
+        unordered_set<int> st;
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                int nums = grid[r+i][c+j];
+                if(nums < 1 || nums > 9 || st.count(nums))
+                    return false;
+                else
+                    st.insert(nums);
+            }
+        }
+
+        int tot_sum = grid[r][c] + grid[r][c+1] + grid[r][c+2];
+
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < 3; j++)
+            {
+                if(grid[r+i][c] + grid[r+i][c+1] + grid[r+i][c+2] != tot_sum)
+                    return false;
+                if(grid[r][c+i] + grid[r+1][c+i] + grid[r+2][c+i] != tot_sum)
+                    return false;
+            }
+        }
+
+        if(grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2] != tot_sum)
+            return false;
+        if(grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != tot_sum)
+            return false;
+        return true;
+    }
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid.size();
+
+        int count = 0;
+
+        for(int i = 0; i <= rows - 3; i++)
+        {
+            for(int j = 0; j <= cols - 3; j++)
+            {
+                if(isMagicGrid(grid,i,j))
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
