a2ecf93ee35e2d338b7cadcb6612512d73240e2b
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-08-15 10:25:56 -0700

    Time: 0 ms (100.00%) | Memory: 12.9 MB (45.72%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    int smallestDistancePair(vector<int>& nums, int k) {
-        //BRUTE FORCE PRIORITY QUEUE
+    int slidingWindow(vector<int>& nums, int d)
+    {
+        int i = 0;
+        int j = 1;
+        int pairCounts = 0;
         int n = nums.size();
-        int maxEle = *max_element(begin(nums), end(nums));
-        vector<int> vec(maxEle+1, 0);
-        for(int i = 0; i < n; i++)
+        while(j < n)
         {
-            for(int j = i+1; j < n; j++)
+            while(nums[j]-nums[i] > d)
             {
-                int diff_bet_pairs = abs(nums[i] - nums[j]);
-                vec[diff_bet_pairs]++;
+                i++;
+            }
+            pairCounts += (j-i);
+            j++;
+        }
+        return pairCounts;
+    }
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(begin(nums), end(nums));
+        int n = nums.size();
+        int low = 0;
+        int high = nums[n-1] - nums[0];
+        int result = 0;
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2;
+
+            int countPair = slidingWindow(nums, mid);
+
+            if(countPair < k)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                result = mid;
+                high = mid-1;
             }
         }
-        for(int i = 0; i < maxEle+1; i++)
-        {
-            k -= vec[i];
-            if(k <= 0)
-            {
-                return i;
-            }
-        }
-        return -1;
+        return result;
     }
 };
