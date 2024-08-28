70b56220da05d15ca026f7672e27329e80a157da
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-08-28 07:24:40 -0700

    Time: 301 ms (32.24%) | Memory: 222.1 MB (14.86%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
+    vector<vector<int>> directions{{1,0},{-1,0},{0,1},{0,-1}};
     bool checkIfIsland(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j)
     {
-        if(i < 0 || i >= grid1.size() || j < 0 || j >= grid1[0].size())
-        {
-            return true;
-        }
+        int n = grid1.size();
+        int m = grid1[0].size();
 
-        if(grid2[i][j] != 1)
-            return true;
+        bool result = true;
+        queue<pair<int,int>> que;
+        que.push({i,j});
         grid2[i][j] = -1;
 
-        int result = (grid1[i][j] == 1);
+        while(!que.empty())
+        {
+            auto [x, y] = que.front();
+            que.pop();
 
-        result = result & checkIfIsland(grid1, grid2, i+1, j);
-        result = result & checkIfIsland(grid1, grid2, i-1, j);
-        result = result & checkIfIsland(grid1, grid2, i, j+1);
-        result = result & checkIfIsland(grid1, grid2, i, j-1);
+            if(grid1[x][y] != 1)
+                result = false;
+            for(vector<int>& dir : directions)
+            {
+                int newX = x+dir[0];
+                int newY = y+dir[1];
 
+                if(newX >= 0 && newX < n && newY >= 0 && newY < m && grid2[newX][newY] == 1)
+                {
+                    grid2[newX][newY] = -1;
+                    que.push({newX, newY});
+                }
+            }
+        }
         return result;
     }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
