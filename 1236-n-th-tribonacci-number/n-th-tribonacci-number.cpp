43ba51a29d5ebc7aaf542a3b02cea30ce52cdc40
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-08-27 19:27:02 -0700

    Time: 2 ms (52.36%) | Memory: 7.4 MB (45.28%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,22 +1,20 @@
 class Solution {
-public: 
+public:
     int t[38];
     int solve(int n)
     {
-        if(n == 0)
+        if(0 == n)
             return 0;
-        if(n == 1 || n == 2)
+        if(1 == n)
             return 1;
-        
-        if(t[n] != -1)
+        if(2 == n)
+            return 1;
+        if(-1 != t[n])
             return t[n];
-        
-        int a = solve(n-1);
-        int b = solve(n-2);
-        int c = solve(n-3);
-        return t[n] = a+b+c;
+        return t[n] = solve(n-3)+solve(n-2)+solve(n-1); 
     }
-    int tribonacci(int n) {
+    int tribonacci(int n) 
+    {
         memset(t, -1, sizeof(t));
         return solve(n);
     }
