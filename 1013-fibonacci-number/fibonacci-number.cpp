2a5a7253418666dddbcaae7229a7c7597684c796
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-08-27 18:51:35 -0700

    Time: 232 ms (16.08%) | Memory: 8.5 MB (10.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
-    int fib(int n) {
+    int solve(int n, int t[])
+    {
+        if(-1 != t[n])
+            return t[n];
         if(0 == n)
             return 0;
         if(1 == n)
             return 1;
-        return fib(n-1)+fib(n-2);
+        return t[n] = fib(n-1)+fib(n-2);
+    }
+    int fib(int n) 
+    {
+        int t[31];
+        memset(t, -1, sizeof(t));
+        return solve(n, t);
     }
 };
