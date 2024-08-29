43218a155d46dd61f81da2ae426b5a0c7a461bdf
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-08-29 10:17:53 -0700

    Time: 0 ms (100.00%) | Memory: 9.5 MB (39.17%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int t[101];
-    int solve(vector<int>& nums, int i, int& n) {
-        if(i >= n)
-            return 0;
-        
-        if(t[i] != -1)
-            return t[i];
-        
-        int take = nums[i] + solve(nums, i+2, n); //steals ith house and moves to i+2 (because we can't steal adjacent)
-        int skip = solve(nums, i+1, n); //skips this house, now we can move to adjacent next house
-        
-        return t[i]=max(take, skip);
-    }
-    
     int rob(vector<int>& nums) {
         int n = nums.size();
-        memset(t, -1, sizeof(t));
-        return solve(nums, 0, n);
+        vector<int> dp(n+1);
+        dp[0] = 0;
+        int maxProfit = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(i == 0)
+            {
+                dp[i+1] = nums[i];
+                maxProfit = max(maxProfit, dp[i+1]);
+                cout << dp[i+1] << " ";
+                continue;
+            }
+            dp[i+1] = max(nums[i]+dp[i-1], maxProfit);
+            cout << dp[i+1] << " ";
+            maxProfit = max(dp[i+1],maxProfit);
+        }
+        return maxProfit;
     }
 };
