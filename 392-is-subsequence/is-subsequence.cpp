ee74f5559dd6af037d0d23c5835a2789e1855604
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-09-03 05:59:26 -0700

    Time: 0 ms (100.00%) | Memory: 7.8 MB (17.35%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -3,9 +3,6 @@ public:
     bool isSubsequence(string s, string t) {
         int n = s.length();
         int m = t.length();
-        
-        if(0 == n)
-            return true;
         int i = 0, j = 0;
         for(int j = 0; j < m; j++)
         {
