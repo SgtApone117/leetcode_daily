ab26d465d4368deeffdbb90018f1ca1499a4ab69
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-09-10 23:22:36 -0700

    Time: 3 ms (41.47%) | Memory: 7.7 MB (44.87%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        return __builtin_popcount(start^goal);
+        int l = 0;
+        int count = 0;
+        while(l < 32)
+        {
+            if((start&(1 << l)) != (goal&(1 << l)))
+                count++;
+            l++;
+        }
+        return count;
     }
 };
