8c9ae7fdec0db2c54905744886a5e35748c9528c
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-09-24 10:45:58 -0700

    Time: 231 ms (84.26%) | Memory: 160.8 MB (56.99%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,28 +1,67 @@
+//using TRIE data structure no need to check the is true part as it is just the length
+struct TrieNode
+{
+    TrieNode* data[10];
+
+    // bool isEnd;
+
+    TrieNode()
+    {
+        // isEnd = false;
+        for(int i = 0; i < 10; i++)
+            data[i] = nullptr;
+    }
+};
+
 class Solution {
+private:
+    void insert(TrieNode* root, int number)
+    {
+        TrieNode* curr = root;
+        string varTr = to_string(number);
+        for(char &ch : varTr)
+        {
+            if(curr->data[ch-'0'] == nullptr)
+            {
+                TrieNode* newNode = new TrieNode();
+                curr->data[ch-'0'] = newNode;
+            }
+            curr = curr->data[ch-'0'];
+        }
+        // curr->isEnd = true;
+    }
+
+    int search(TrieNode* root, int number)
+    {
+        TrieNode* curr = root;
+        string varTr = to_string(number);
+        int count = 0;
+        for(char &ch : varTr)
+        {
+            if(curr->data[ch-'0'] == nullptr)
+            {
+                break;
+            }
+            else
+            {
+                count++;
+                curr = curr->data[ch-'0'];
+            }
+        }
+        return count;
+    }
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<int> st;
-        for(int &val : arr1)
+        TrieNode* root = new TrieNode();
+        int res = 0;
+        for(int &num : arr1)
         {
-            while(!st.count(val) && val > 0)
-            {
-                st.insert(val);
-                val /= 10;
-            }
+            insert(root, num);
         }
-
-        int result = 0;
         for(int &num : arr2)
         {
-            while(!st.count(num) && num > 0)
-            {
-                num /= 10;
-            }
-            if(num > 0)
-            {
-                result = max(result, static_cast<int>(log10(num)+1));
-            }
+            res = max(res, search(root, num));
         }
-        return result;
+        return res;
     }
 };
