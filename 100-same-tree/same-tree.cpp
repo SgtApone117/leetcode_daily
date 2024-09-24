65f4f31f3072ed4f8be06f8f24e5429c43f0f9a0
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-09-23 18:24:43 -0700

    Time: 0 ms (100.00%) | Memory: 12.2 MB (39.70%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -16,36 +16,8 @@ public:
             return true;
         if(p == NULL || q == NULL)
             return false;
-        queue<TreeNode*> que1;
-        queue<TreeNode*> que2;
-
-        que1.push(p);
-        que2.push(q);
-
-        while(!que1.empty() && !que2.empty())
-        {
-            TreeNode* node1 = que1.front();
-            que1.pop();
-            TreeNode* node2 = que2.front();
-            que2.pop();
-
-            if(node1->val != node2->val)
-                return false;
-            if(node1->left != NULL && node2->left != NULL)
-            {
-                que1.push(node1->left);
-                que2.push(node2->left);
-            }
-            else if(node1->left != NULL || node2->left != NULL)
-                return false;
-            if(node1->right != NULL && node2->right != NULL)
-            {
-                que1.push(node1->right);
-                que2.push(node2->right);
-            }
-            else if(node1->right != NULL || node2->right != NULL)
-                return false;
-        }
-        return true;
+        if(p->val != q->val)
+            return false;
+        return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     }
 };
