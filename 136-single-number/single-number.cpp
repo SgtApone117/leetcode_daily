cf19ff17d142c6ccb24fbc93519153646b76b8c3
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-11-11 15:55:18 -0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (65.80%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int XOR = 0;
-        for(int &num : nums)
+        for(int num : nums)
         {
             XOR ^= num;
         }
