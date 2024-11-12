e46da30ea40e42190f2b1773529a512b339d1e62
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-11-11 20:25:22 -0800

    Time: 0 ms (100.00%) | Memory: 7.6 MB (26.63%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n == 0)
-		return false;
-
-	return (ceil(log2(n)) == floor(log2(n)));
+        return (n > 0) && ((n & n-1) == 0);
     }
 };
