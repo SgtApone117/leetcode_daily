b2f8f0af56e39e2dc521a3499137221febc3a9ff
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-11-24 14:58:34 -0800

    Time: 7 ms (11.82%) | Memory: 38.4 MB (31.40%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -6,14 +6,16 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         int get_min = INT_MAX;
+        
         ll total_sum = 0;
         for(int row = 0; row < m; ++row)
         {
+            int *p = &matrix[row][0];
             for(int col = 0; col < n; ++col)
             {
-                get_min = min(get_min, abs(matrix[row][col]));
-                total_sum += abs(matrix[row][col]);
-                if(matrix[row][col] < 0)
+                get_min = min(get_min, abs(*(p + col)));
+                total_sum += abs(*(p + col));
+                if(*(p + col) < 0)
                 {                    
                     count_negve++;
                 }
