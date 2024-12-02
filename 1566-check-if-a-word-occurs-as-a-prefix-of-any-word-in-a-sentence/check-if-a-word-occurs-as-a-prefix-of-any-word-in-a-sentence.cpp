b2fda07ceb0420d978075cbc89440b98e4d3ace9
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-12-01 16:24:06 -0800

    Time: 0 ms (100.00%) | Memory: 8.2 MB (7.80%) - LeetSync

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        stringstream ss(sentence);
+        string word;
+        int count = 0;
+        while(ss >> word)
+        {
+            size_t found = word.find(searchWord);
+            if(found != string::npos)
+            {
+                cout << found << endl;
+                if(found == 0)
+                {
+                    count++;
+                    return count;
+                }
+            }
+            count++;
+        }
+        return -1;
+    }
+};
