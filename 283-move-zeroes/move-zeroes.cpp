53464d7aa70a88c8aee479ef98bcba18d8fbc79c
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2024-12-29 16:35:54 -0800

    Time: 0 ms (100.00%) | Memory: 24 MB (10.49%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,33 +2,16 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int i = 0;
-        int j = 0;
-        while(j < n)     
+        int i = 0; // point to zero
+        int j = 0; // keep moving ahead and swaps when non zero is encountered
+        while(j < n)
         {
             if(nums[j] != 0)
             {
-                swap(nums[i], nums[j]);
+                swap(nums[j], nums[i]);
                 i++;
-                j++;
             }
-            else
-            {
-                j++;
-            }
+            j++;
         }
     }
 };
-
-// 2 1 i = 0 and j = 1
-/*
-    i and j both are non zero so update
-
-    nums[i]
-*/
-
-// 0 1 0 3 12 i and j 0th and 1st position 
-// 1 0 0 3 12 i = 1 j = 2
-// 1 0 0 3 12 i = 1 j = 3
-// 1 3 0 0 12 i = 2 j = 4
-// 1 3 12 0 0 i = 3 j = 5
