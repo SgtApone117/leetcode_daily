c867939e06ca4e77383846b6d546a3f89dc6de43
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2025-01-14 19:00:24 -0800

    Time: 3 ms (39.30%) | Memory: 17 MB (70.89%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
-    int operate(int &a, int &b, const string &s) {
-        if(s == "+")
-            return a+b;
-        
-        if(s == "-")
-            return a-b;
-        
-        if(s == "*")
-            return (long)a*(long)b;
-        
-        if(s == "/")
-            return a/b;
-        
-        return -1;
-    }
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
-        int result = 0;
-        for(const string& s:tokens) {
-            if(s == "+" || s == "-" || s == "*" || s == "/") {
-                int b = st.top();
+        for(string& ch : tokens)
+        {
+            if(ch == "+")
+            {
+                int val1 = st.top();
                 st.pop();
-                int a = st.top();
+                int val2 = st.top();
                 st.pop();
-                
-                result = operate(a, b, s);
-                st.push(result);
-            } else {
-                st.push(stoi(s));
+                st.push(val2+val1);
             }
+            else if(ch == "-")
+            {
+                int val1 = st.top();
+                st.pop();
+                int val2 = st.top();
+                st.pop();
+                st.push(val2-val1);
+            }
+            else if(ch == "*")
+            {
+                int val1 = st.top();
+                st.pop();
+                int val2 = st.top();
+                st.pop();
+                st.push(val2*val1);
+            }
+            else if(ch == "/")
+            {
+                int val1 = st.top();
+                st.pop();
+                int val2 = st.top();
+                st.pop();
+                st.push(val2/val1);
+            }
+            else
+                st.push(stoi(ch));
         }
         return st.top();
     }
