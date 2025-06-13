d6e688acaf483e7d0ca0f0706d9adc68dc3c4884
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2025-06-13 13:53:45 -0700

    Time: 2 ms (30.97%) | Memory: 61.1 MB (53.44%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -4,7 +4,7 @@
  */
 var reverseString = function(s) {
     n = s.length
-    for(let i = 0; i < n/2; i++) {
+    for(let i = 0; i < Math.floor(n/2); i++) {
         let temp = s[i];
         s[i] = s[n-1-i]; 
         s[n-1-i] = temp;
