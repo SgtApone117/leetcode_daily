cdbed0c248994a5aab2e2519a86a1cc056b30057
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2025-06-25 15:30:32 -0700

    Time: 1 ms (55.21%) | Memory: 61.1 MB (52.16%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,10 +3,11 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    n = s.length
-    for(let i = 0; i < Math.floor(n/2); i++) {
+    n = s.length;
+    for(let i = 0; i < n/2; i++) {
         let temp = s[i];
-        s[i] = s[n-1-i]; 
-        s[n-1-i] = temp;
+        s[i] = s[n-i-1];
+        s[n-i-1] = temp;
     }
+    return s;
 };
