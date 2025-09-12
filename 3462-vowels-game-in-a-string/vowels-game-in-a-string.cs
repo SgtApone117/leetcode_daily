42b2f1334b9ba5bc4582d53977578fa6c7419f1d
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2025-09-12 15:55:20 -0700

    Time: 3 ms (100.00%) | Memory: 49.8 MB (69.23%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool DoesAliceWin(string s) {
+        foreach(var ch in s)
+        {
+            if (ch == 'a' ||
+               ch == 'e' ||
+               ch == 'i' ||
+               ch == 'o' ||
+               ch == 'u')
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
