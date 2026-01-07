1f5b5921cf391a59574d4cf49a1f5400ce5fd679
Sumeet Khedkar <Sumeetarbiter117@gmail.com>
2026-01-07 15:52:24 -0800

    Time: 5 ms (38.48%) | Memory: 109.6 MB (29.27%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.cpp
@@ -10,94 +10,45 @@
  * };
  */
 class Solution {
-private:
-    int levelOfTree(TreeNode* root) //recursion
+public:
+    int solve(TreeNode* root)
     {
         if(root == nullptr)
         {
             return 0;
         }
-        int leftLevel  = levelOfTree(root->left); 
-        int rightLevel = levelOfTree(root->right);
-        return 1 + max(leftLevel,rightLevel);
-    }    
-
-    int levelOfTreeQueue(TreeNode* root)
-    {
-        if(root == nullptr)
-        {
-            return 0;
-        }
-
-        queue<TreeNode*> q;
-        q.push(root);
-        int level = 0;
-
-        while(!q.empty())
-        {
-            int size = q.size();
-            level = level + 1;
-
-            for(int i = 0; i < size; i++)
-            {
-                TreeNode* node = q.front();
-                q.pop();
-                if(node->left != nullptr)
-                {
-                    q.push(node->left);
-                }
-                if(node->right != nullptr)
-                {
-                    q.push(node->right);
-                }
-            }
-        }
-        return level;
-    }
-
-    int sumAtEachLevel(TreeNode* root)
-    {
-        if(root == nullptr)
-        {
-            return 0;
-        }
-        pair<int,int> res = make_pair(1,root->val);
-        //vector<int> result;
+        pair<int,int> res = {1, root->val};
         queue<TreeNode*> q;
         q.push(root);
         int level = 0;
         while(!q.empty())
         {
             int size = q.size();
-            level = 1 + level;
             int sum = 0;
-
+            level = level + 1;
             for(int i = 0; i < size; i++)
             {
                 TreeNode* node = q.front();
                 q.pop();
                 sum = sum + node->val;
-                if(node->left != nullptr)
+                if(node->left)
                 {
                     q.push(node->left);
                 }
-                if(node->right != nullptr)
+                if(node->right)
                 {
                     q.push(node->right);
                 }
-                
             }
             if(res.second < sum)
             {
                 res.first = level;
                 res.second = sum;
             }
-            //result.push_back(sum);
         }
         return res.first;
     }
-public:
     int maxLevelSum(TreeNode* root) {
-        return sumAtEachLevel(root);
+        return solve(root);
     }
 };
